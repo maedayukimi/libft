4262cf74a53d41920bb46d9eac2fe33c58ced086
Masatada Wako <mawako@c1r3s3.42tokyo.jp>
2024-09-08 18:52:44 +0900

    Hello, Kitty, how about...

---
+++ ft_strnstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mawako <mawako@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/24 11:38:29 by mawako            #+#    #+#             */
+/*   Updated: 2024/09/08 18:39:30 by mawako           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *str, const char *to_find, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	char	*s;
+
+	s = (char *)str;
+	if (!str)
+		return (0);
+	if (*to_find == 0)
+		return (s);
+	i = 0;
+	while (str[i] && i < len)
+	{
+		j = 0;
+		while (to_find[j] && str[i + j] == to_find[j])
+			j++;
+		if (i + j <= len && to_find[j] == 0)
+			return (s + i);
+		i++;
+	}
+	return (0);
+}
